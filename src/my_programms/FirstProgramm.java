4bd3c8ab7ad343374e51b33b95574d204f5be751
Wretchman <hab4791@gmail.com>
2018-05-25 15:47:37 -0400

    Some program code added

---
+++ src/my_programms/FirstProgramm.java
@@ -0,0 +1,12 @@
+package my_programms;
+
+public class FirstProgramm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Welcome to Java Programming");
+		System.out.println("This is my first java programming");
+	}
+
+}
