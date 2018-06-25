98b2b3ba9970f341274f7e3fcafd561f408f2e37
Wretchman <hab4791@gmail.com>
2018-06-24 22:02:17 -0400

    This is my another commit

--- src/my_programms/Temperature.java
+++ src/my_programms/Temperature.java
@@ -10,7 +10,7 @@ public class Temperature {
 		System.out.println("Give in the celsius value:");
 		Scanner sc = new Scanner(System.in);
 		double celsius = sc.nextDouble();
-		double fahrenheit = 9.0 / 5.0 * celsius  + 32.0;
+		double fahrenheit = 19.0 / 5.0 * celsius  + 32.0;
 		
 		System.out.println("Fahrenheit: "+ fahrenheit);
 		
